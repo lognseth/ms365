00476f0b9b80ae0aa5323a7f0fb585348284623f
Mikael Lognseth <mikael.lognseth@arribatec.com>
2021-04-16 14:20:22 +0200

    Enables Passwordless for all tenants under partner account

---
+++ Azure AD/Enable-PasswordLess.ps1
@@ -0,0 +1,34 @@
+﻿######### Secrets #########
+$ApplicationId = 'YourAPPID'
+$ApplicationSecret = 'YourAppPassword' | Convertto-SecureString -AsPlainText -Force
+$TenantID = 'YourTenantID'
+$RefreshToken = 'Refreshtoken'
+$UPN = "Valid-upn"
+######### Secrets #########
+$credential = New-Object System.Management.Automation.PSCredential($ApplicationId, $ApplicationSecret)
+
+$aadGraphToken = New-PartnerAccessToken -ApplicationId $ApplicationId -Credential $credential -RefreshToken $refreshToken -Scopes 'https://graph.windows.net/.default' -ServicePrincipal -Tenant $tenantID
+$graphToken = New-PartnerAccessToken -ApplicationId $ApplicationId -Credential $credential -RefreshToken $refreshToken -Scopes 'https://graph.microsoft.com/.default' -ServicePrincipal -Tenant $tenantID
+
+Install-module AzureADPreview -AllowClobber
+import-module AzureADPreview
+Connect-AzureAD -AadAccessToken $aadGraphToken.AccessToken -AccountId $upn -MsAccessToken $graphToken.AccessToken -TenantId $tenantID | Out-Null
+$tenants = Get-AzureAdContract -All:$true
+Disconnect-AzureAD
+
+foreach ($tenant in $tenants) {
+
+    write-host "Working on client $($tenant.defaultdomainname)"
+    try {
+        $CustAadGraphToken = New-PartnerAccessToken -ApplicationId $ApplicationId -Credential $credential -RefreshToken $refreshToken -Scopes "https://graph.windows.net/.default" -ServicePrincipal -Tenant $tenant.CustomerContextId
+        $CustGraphToken = New-PartnerAccessToken -ApplicationId $ApplicationId -Credential $credential -RefreshToken $refreshToken -Scopes "https://graph.microsoft.com/.default" -ServicePrincipal -Tenant $tenant.CustomerContextId
+        Connect-AzureAD -AadAccessToken $CustAadGraphToken.AccessToken -AccountId $upn -MsAccessToken $CustGraphToken.AccessToken -TenantId $tenant.CustomerContextId | out-null
+        $exists = Get-AzureADPolicy | Where-Object -Property type -eq AuthenticatorAppSignInPolicy
+        if ($exists) { write-host  "Policy exists for $($tenant.DefaultDomainName)"; continue }
+        New-AzureADPolicy -Type AuthenticatorAppSignInPolicy -Definition '{"AuthenticatorAppSignInPolicy":{"Enabled":true}}' -isOrganizationDefault $true -DisplayName 'PasswordlessSignin'
+    }
+    catch {
+        Write-Warning "Could not log into tenant $($tenant.DefaultDomainName) or retrieve policy. Error: $($_.Exception.Message)"
+    }
+
+}
--- Exchange/ChangeUPN.ps1
+++ Exchange/ChangeUPN.ps1
@@ -1,3 +1,6 @@
+
+#Definer gammelt og nytt UPN i riktig Array (pass på at rekkefølgen er lik.)
+
 $OldUPN = @(
     "bruker@whatever.no"
     "bruker2@whatever.no"
@@ -8,6 +11,8 @@ $NewUPN = @(
     "bruker2@whatever.de"
 )
 
+#Koble til Azure AD og Exchange Online
+
 Connect-MsolService
 Connect-ExchangeOnline
 
@@ -15,6 +20,14 @@ foreach ($user in $users) {
     Set-MsolUserPrincipalName -UserPrincipalName $OldUpn -NewUserPrincipalName $NewUpn -WhatIf
 }
 
+Start-Sleep -s 5
+
 foreach ($user in $users) {
     Set-Mailbox -identity $NewUPN -WindowsEmailAddress $NewUPN -WhatIf
 }
+
+#Hvis overnente ikke fungerer kan du også bare kjøre det som en one-liner:
+
+# Set-MsolUserPrincipalName -UserPrincipalName navn@domene.no -NewUserPrincipalName navn@domene.no
+
+# Set-Mailbox -identity navn@domene.no -WindowsEmailAddress navn@domene.no
